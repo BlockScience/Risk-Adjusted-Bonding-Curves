53e40c73e2a79bb65a820cc92acaf3eeae0de4ad
ShrutiAppiah <shruti.appiah@gmail.com>
2020-06-29 19:56:12 +0300

    updated file structure - works w/ 0.4.15

---
+++ Model/src/__init__.py
@@ -0,0 +1,11 @@
+#import os
+#import sys
+#path = os.path.join(os.path.dirname(__file__), os.pardir)
+#sys.path.append(path)
+
+#from Model.src.sim import config as sys_model
+
+#import os
+#import sys
+#path = os.path.join(os.path.dirname(__file__), os.pardir)
+#sys.path.append(path)
