0fd078a22bf6d76cff5da90b6b3e935653ffb6ee
ShrutiAppiah <shruti.appiah@gmail.com>
2020-09-15 16:36:04 +0300

    updated plots and random drop

--- src/sim/model/parts/bondburn.py
+++ src/sim/model/parts/bondburn.py
@@ -94,7 +94,7 @@ def compute_s_free(R, S, V, kappa, s_free, deltaR, policy_input, timestep):
     s_free = s_free + deltas - policy_input['amt_to_burn']
 
     # TEST RANDOM DROP
-    if timestep % 20 == 0:
+    if timestep % 2 == 0:
         random_drop = 10
     else:
         random_drop = 0
--- src/sim/model/parts/private_beliefs.py
+++ src/sim/model/parts/private_beliefs.py
@@ -120,18 +120,18 @@ def update_agent_beliefs(params, substep, state_history, prev_state, policy_inpu
     #new_private_price = agent['agent_private_price']
     #new_private_alpha = agent['agent_private_alpha']
     
-    b_alpha = 0.3 # bias
+    b_alpha = 0.0 # bias
 
-    public_alpha_signal = 0.5 - ((1/1000)*timestep)
-    private_alpha_signal = 0.5 - ((1/1000)*timestep)
+    public_alpha_signal = 0.9
+    private_alpha_signal = 0.8 
     #private_alpha_signal = 0.5 - ((1/1000)*timestep)
 
     private_alpha = (b_alpha)*public_alpha_signal + (1-b_alpha)*private_alpha_signal
 
-    b_price = 0.3 # bias
+    b_price = 0.0 # bias
 
-    public_price_signal = 0.5 - ((1/1000)*timestep)
-    private_price_signal = 0.5 - ((1/1000)*timestep)
+    public_price_signal = 0.9
+    private_price_signal = 0.8 
     #private_price_signal = 1.5 - ((1/1000)*timestep)
 
     private_price = (b_price)*public_price_signal + (1-b_price)*private_price_signal
