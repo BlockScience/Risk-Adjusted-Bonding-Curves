ff7ab74d3acfe5922f98fb11d1d61395861880d7
Matt Barlin <33430774+matttyb80@users.noreply.github.com>
2020-11-03 09:39:26 -0500

    update payout and profit

--- Code_With_Us/src/sim/model/parts/utils.py
+++ Code_With_Us/src/sim/model/parts/utils.py
@@ -102,6 +102,10 @@ def price(experiments,test_title,T):
 
 
 def agent_payout(experiments,t):
+    """
+    For CWU Payout
+    """
+
     S_free = experiments.supply_free[t]
     S_0 = experiments.supply_0[t]
     S_1 = experiments.supply_1[t]
@@ -118,17 +122,18 @@ def agent_payout(experiments,t):
         Q0 = experiments.attestations_0[t]
         Q1 = 1 
         R = experiments.reserve[t]
-        S = experiments.supply[t]
+        S = experiments.supply[t] - 1000000 # subtract initial amount
         C = 300000000 
         alpha = experiments.alpha[t]
-        if alpha < 0.5:
+        if alpha < 0.4:
             alpha = 0
-        elif alpha > 0.5:
+        elif alpha >= 0.4:
             alpha = 1
         T1 = (s_free/S)*(C*alpha + R)
-        T2 = (s1/(S-S_0))*alpha*(C+R)
-        T3 = (s0/(S-S_1))*(1-alpha)*(R)
-        agent_payout = T1+T2+T3
+        # T2 = (s1/(S-S_0))*alpha*(C+R)
+        # T3 = (s0/(S-S_1))*(1-alpha)*(R)
+
+        agent_payout = T1
         payout_list.append(agent_payout)
 
         arr2d = np.array(payout_list)
@@ -150,13 +155,62 @@ def agent_payout(experiments,t):
 
     plt.show()
 
+# def agent_payout(experiments,t):
+#     S_free = experiments.supply_free[t]
+#     S_0 = experiments.supply_0[t]
+#     S_1 = experiments.supply_1[t]
+#     agents_id = [0,1,2,3,4,5,6,7,9]
+#     payout_list = []
+#     for a in agents_id:
+#         q1 = experiments.agents[t].agent_attestations_1[a]
+#         q0 = experiments.agents[t].agent_attestations_0[a]
+#         s_free = experiments.agents[t].agent_supply_free[a]
+#         s1 = experiments.agents[t].agent_supply_1[a]
+#         s0 = experiments.agents[t].agent_supply_0[a]
+#         s = s_free + s1 + s0
+#         agent_private_alpha = experiments.agents[t].agent_private_alpha[a]
+#         Q0 = experiments.attestations_0[t]
+#         Q1 = 1 
+#         R = experiments.reserve[t]
+#         S = experiments.supply[t]
+#         C = 300000000 
+#         alpha = experiments.alpha[t]
+#         if alpha < 0.5:
+#             alpha = 0
+#         elif alpha > 0.5:
+#             alpha = 1
+#         T1 = (s_free/S)*(C*alpha + R)
+#         T2 = (s1/(S-S_0))*alpha*(C+R)
+#         T3 = (s0/(S-S_1))*(1-alpha)*(R)
+#         agent_payout = T1+T2+T3
+#         payout_list.append(agent_payout)
+
+#         arr2d = np.array(payout_list)
+
+#     arr1d = arr2d.flatten()
+
+#     x = agents_id
+#     payouts = arr1d
+
+#     x_pos = [i for i, _ in enumerate(x)]
+
+#     fig = plt.figure(figsize=(15, 10))
+#     plt.bar(x_pos, payouts, color='green')
+#     plt.xlabel("Agent ID")
+#     plt.ylabel("Payout amount (uXCHF)")
+#     plt.title("Agent and their Payouts")
+
+#     plt.xticks(x_pos, x)
+
+#     plt.show()
+
     
     
 def agent_ROI(experiments,t):
     S_free = experiments.supply_free[t]
     S_0 = experiments.supply_0[t]
     S_1 = experiments.supply_1[t]
-    agents_id = [0,1,2,3,4,5,6,7,9]
+    agents_id = [0,1,2,3,4,5,6,7,8,9]
     payout_list = []
     roi = []
     for a in agents_id:
@@ -206,11 +260,62 @@ def agent_ROI(experiments,t):
     plt.show()
 
     
+# def agent_profit(experiments,t):
+#     S_free = experiments.supply_free[t]
+#     S_0 = experiments.supply_0[t]
+#     S_1 = experiments.supply_1[t]
+#     agents_id = [0,1,2,3,4,5,6,7,9]
+#     payout_list = []
+#     profits = []
+#     for a in agents_id:
+#         q1 = experiments.agents[t].agent_attestations_1[a]
+#         q0 = experiments.agents[t].agent_attestations_0[a]
+#         s_free = experiments.agents[t].agent_supply_free[a]
+#         s1 = experiments.agents[t].agent_supply_1[a]
+#         s0 = experiments.agents[t].agent_supply_0[a]
+#         s = s_free + s1 + s0
+#         agent_private_alpha = experiments.agents[t].agent_private_alpha[a]
+#         Q0 = experiments.attestations_0[t]
+#         Q1 = 1 
+#         R = experiments.reserve[t]
+#         S = experiments.supply[t]
+#         C = 300000000 
+#         alpha = experiments.alpha[t]
+#         if alpha < 0.5:
+#             alpha = 0
+#         elif alpha > 0.5:
+#             alpha = 1
+#         T1 = (s_free/S)*(C*alpha + R)
+#         T2 = (s1/(S-S_0))*alpha*(C+R)
+#         T3 = (s0/(S-S_1))*(1-alpha)*(R)
+#         agent_payout = T1+T2+T3
+#         payout_list.append(agent_payout)
+#         profits.append(agent_payout - 14000000)
+
+#         arr2d = np.array(profits)
+
+#     arr1d = arr2d.flatten()
+
+#     x = agents_id
+#     profit = arr1d
+
+#     x_pos = [i for i, _ in enumerate(x)]
+
+#     fig = plt.figure(figsize=(15, 10))
+#     plt.bar(x_pos, profit, color='green')
+#     plt.xlabel("Agent ID")
+#     plt.ylabel("Profit")
+#     plt.title("Agents and their Profits")
+
+#     plt.xticks(x_pos, x)
+
+#     plt.show()
+
 def agent_profit(experiments,t):
     S_free = experiments.supply_free[t]
     S_0 = experiments.supply_0[t]
     S_1 = experiments.supply_1[t]
-    agents_id = [0,1,2,3,4,5,6,7,9]
+    agents_id = [0,1,2,3,4,5,6,7,8,9]
     payout_list = []
     profits = []
     for a in agents_id:
@@ -224,17 +329,17 @@ def agent_profit(experiments,t):
         Q0 = experiments.attestations_0[t]
         Q1 = 1 
         R = experiments.reserve[t]
-        S = experiments.supply[t]
+        S = experiments.supply[t] - 1000000 # subtract initial amount
         C = 300000000 
         alpha = experiments.alpha[t]
-        if alpha < 0.5:
+        if alpha < 0.4:
             alpha = 0
-        elif alpha > 0.5:
+        elif alpha > 0.4:
             alpha = 1
         T1 = (s_free/S)*(C*alpha + R)
-        T2 = (s1/(S-S_0))*alpha*(C+R)
-        T3 = (s0/(S-S_1))*(1-alpha)*(R)
-        agent_payout = T1+T2+T3
+        # T2 = (s1/(S-S_0))*alpha*(C+R)
+        # T3 = (s0/(S-S_1))*(1-alpha)*(R)
+        agent_payout = T1
         payout_list.append(agent_payout)
         profits.append(agent_payout - 14000000)
 
